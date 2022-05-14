2771e409c3d421199721662ac270ffd8a6390e95
chamara <lasithachamara0@gmail.com>
2022-05-14 23:40:42 +0530

    add

--- app/src/main/java/com/example/cocoandtoto/Login.java
+++ app/src/main/java/com/example/cocoandtoto/Login.java
@@ -1,24 +1,92 @@
 package com.example.cocoandtoto;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity  {
 
     private Button signup;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://coco1-baa44-default-rtdb.firebaseio.com/");
+
+    private TextView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        signup = (Button) findViewById(R.id.btn_signup);
+        final Button LogButton = findViewById(R.id.LogButton);
+        final TextInputEditText Password = findViewById(R.id.Password);
+        final TextInputEditText mail = findViewById(R.id.mail);
 
-        signup.setOnClickListener(new View.OnClickListener() {
+        backButton = findViewById(R.id.backButton);
+
+        LogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String mailtext = mail.getText().toString();
+                final String Passwordtext = Password.getText().toString();
+
+                if(mailtext.isEmpty() || Passwordtext.isEmpty()){
+                    Toast.makeText(Login.this, "Please enter Your user name or password", Toast.LENGTH_SHORT).show();
+                }else {
+                    databaseReference.child("Profile").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            //check if username
+                            if(snapshot.hasChild(mailtext)){
+                                final String getPassword = snapshot.child(mailtext).child("pass").getValue(String.class);
+
+                                if(getPassword.equals(Passwordtext)){
+                                    Toast.makeText(Login.this, "Successfully Logged in", Toast.LENGTH_SHORT).show();
+                                    //startActivity(new Intent(LoginActivity.this,Shop.class));
+
+                                    Intent intent = new Intent(Login.this,Recycle_view.class);
+
+                                    startActivity(intent);
+
+                                }
+                                else{
+                                    Toast.makeText(Login.this, "Wrong Password", Toast.LENGTH_SHORT).show();
+                                }
+                            }else{
+                                Toast.makeText(Login.this, "Wrong Password", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                }
+
+            }
+        });
+
+
+
+
+
+
+
+
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Login.this, form_1.class);
--- app/src/main/java/com/example/cocoandtoto/form_1.java
+++ app/src/main/java/com/example/cocoandtoto/form_1.java
@@ -13,40 +13,88 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class form_1 extends AppCompatActivity implements View.OnClickListener{
-
-    private EditText nameC,mail1C,addressC,contactC,passC;
-    private TextView CoConToTo,btn_sub;
+public class form_1 extends AppCompatActivity {
 
 
-    private FirebaseAuth mAuth;
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://coco1-baa44-default-rtdb.firebaseio.com/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form1);
 
-        mAuth =FirebaseAuth.getInstance();
+        final TextInputEditText c_name = findViewById(R.id.c_name);
+        final TextInputEditText c_address = findViewById(R.id.c_address);
+        final TextInputEditText c_mail1 = findViewById(R.id.c_mail1);
+        final TextInputEditText c_contact = findViewById(R.id.c_contact);
+        final TextInputEditText c_pass = findViewById(R.id.c_pass);
+        final Button c_btn_sub = findViewById(R.id.c_btn_sub);
 
-        CoConToTo = (TextView) findViewById(R.id.CoConToTo);
-        CoConToTo.setOnClickListener(this);
+        c_btn_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        btn_sub = (Button) findViewById(R.id.c_btn_sub);
-        btn_sub.setOnClickListener(this);
+                final String nametext = c_name.getText().toString();
+                final String Adresstext = c_address.getText().toString();
+                final String mailtext = c_mail1.getText().toString();
+                final String contacttext = c_contact.getText().toString();
+                final String passtext = c_pass.getText().toString();
 
-        nameC = (EditText) findViewById(R.id.c_name);
-        addressC = (EditText) findViewById(R.id.c_address);
-        mail1C = (EditText) findViewById(R.id.c_mail1);
-        contactC = (EditText) findViewById(R.id.c_contact);
-        passC = (EditText) findViewById(R.id.c_pass);
+                if(nametext.isEmpty() || Adresstext.isEmpty() || mailtext.isEmpty() || contacttext.isEmpty() ||passtext.isEmpty() ){
+                    Toast.makeText(form_1.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+                }
+              //  else if (passtext.length()<5){
+                //    Toast.makeText(form_1.this, "Minimum password length should be 5 characters", Toast.LENGTH_SHORT).show();
+               // }
+                else {
+                    databaseReference.child("Profile").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.hasChild(mailtext)){
+                                Toast.makeText(form_1.this, "User name is already registered", Toast.LENGTH_SHORT).show();
+                            }else {
+                                databaseReference.child("Profile").child(mailtext).child("Name").setValue(nametext);
+                                databaseReference.child("Profile").child(mailtext).child("adress").setValue(Adresstext);
+                                databaseReference.child("Profile").child(mailtext).child("mail").setValue(mailtext);
+                                databaseReference.child("Profile").child(mailtext).child("contact").setValue(contacttext);
+                                databaseReference.child("Profile").child(mailtext).child("pass").setValue(passtext);
+
+                                Toast.makeText(form_1.this, "User register Successfully", Toast.LENGTH_SHORT).show();
+                                finish();
+
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+
+
+                }
+
+            }
+        });
+
 
 
 
     }
+}
+
+/* }
 
     @Override
     public void onClick(View v) {
@@ -132,7 +180,4 @@ public class form_1 extends AppCompatActivity implements View.OnClickListener{
                 }
 
             }
-        });
-
-    }
-}
+        });*/
